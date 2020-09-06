0bf5e242b372d835731c0774c2974910cf633290
Voon <voonyn@gmail.com>
2020-09-06 11:04:36 +0800

    Init project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home.vue'
+import FacemaskDetection from '../components/FacemaskDetection.vue'
 
 Vue.use(VueRouter)
 
@@ -11,15 +12,10 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+    path: '/facemask-detection',
+    name: 'FacemaskDetection',
+    component: FacemaskDetection
+  },
 ]
 
 const router = new VueRouter({
